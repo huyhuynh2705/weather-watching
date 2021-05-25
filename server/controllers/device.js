108b463a2c7db706cad08dc841283a0d866f3688
ShadowHuynh <thaihuya42000@gmail.com>
2021-05-26 01:13:26 +0700

    complete update, delete device

--- server/controllers/device.js
+++ server/controllers/device.js
@@ -64,11 +64,80 @@ export const getCountDevice = async (req, res) => {
 export const updateDevice = async (req, res) => {
     
     // req.body = {id: '', type: '', idServer: '', name: '', unit: '', topic: ''}
+    const { id, type, idServer, name, unit, topic} = req.body;
 
+    const oldDevice = await DeviceModel.findById(id);
+
+    //san pham khong ton tai
+    if (!oldDevice) {
+        return res.status(404).json({ message: "Device doesn't exist."});
+    }
+
+    //check if nothing is changed
+    if ( ((type==oldDevice.type)||(type='')) && ((idServer==oldDevice.idServer)||(idServer==''))
+        && ((name==oldDevice.name)||(name=='')) && ((unit==oldDevice.unit)||(unit=='')) && 
+        ((topic==oldDevice.topic)||(topic=='')) ) 
+    {
+        return res.status(200).json({ message: "Nothing was changed."});
+    }
+
+    if (type != oldDevice.type) {
+        if (type == '') {
+            type = oldDevice.type;
+        }
+    }
+    
+    if (idServer != oldDevice.idServer) {
+        if (idServer == '') {
+            idServer = oldDevice.idServer;
+        }
+    }
+
+    if (name != oldDevice.name) {
+        if (name == '') {
+            name = oldDevice.name;
+        }
+    }
+
+    if (unit != oldDevice.unit) {
+        if (unit == '') {
+            unit = oldDevice.unit;
+        }
+    }
+
+    if (topic != oldDevice.topic) {
+        if (topic == '') {
+            topic = oldDevice.topic;
+        }
+    }
+
+    let updateDevice = { type, idServer, name, unit, topic };
+
+    const updatedDevice = await DeviceModel.findByIdAndUpdate(id, updateDevice, { new: true });
+
+    //res.json({ result: updatedDevice });
+    //?????????????
+    res.status(200).json(updatedDevice);
 }
 
 export const deleteDevice = async (req, res) => {
     
     // req.params= {id: ''}
+    const { id } = req.params;
+
+    try {
+        const oldDevice = await DeviceModel.findById(id);
+        if (!oldDevice) {
+            return res.status(404).json({ message: "Device doesn't exist." });
+        }
+        else {
+            await DeviceModel.findByIdAndRemove(id);
+
+            return res.status(200).json({ message: "Device is deleted."});            
+        }
+        
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 
 }
